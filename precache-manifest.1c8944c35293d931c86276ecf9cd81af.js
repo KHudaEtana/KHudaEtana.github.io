a2f8953b5dac6adf11f6ffa65f7574206421dcfa
Huda <khairul.nasution@id.etanabiotech.com>
2022-02-21 13:27:36 +0700

    test for mockup

---
+++ precache-manifest.1c8944c35293d931c86276ecf9cd81af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb8781208627eb8fb5ee3993a6d8f5ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "855cef4470d96e290402",
+    "url": "/static/css/main.73f9653c.chunk.css"
+  },
+  {
+    "revision": "8774bf5738f26e4221d7",
+    "url": "/static/js/2.b7e9e326.chunk.js"
+  },
+  {
+    "revision": "855cef4470d96e290402",
+    "url": "/static/js/main.36c6eb09.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1cb2cefd7a30679707246804d7cef4ad",
+    "url": "/static/media/Daftar-Perusahaan-BUMN.1cb2cefd.jpg"
+  },
+  {
+    "revision": "477ad830876a31aae6e53c4483906848",
+    "url": "/static/media/Etana_logo.477ad830.png"
+  }
+]);
